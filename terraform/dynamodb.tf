f0dfc2021600b69ececac2ca853fb38ffff61dc2
pkchamps <rixbali057@gmail.com>
2026-06-19 22:53:31 +0530

    Add Terraform state lock table

---
+++ terraform/dynamodb.tf
@@ -0,0 +1,17 @@
+resource "aws_dynamodb_table" "terraform_lock" {
+
+  name         = "terraform-lock-table"
+
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "terraform-lock-table"
+  }
+}
